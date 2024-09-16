c9d827e942e75332cbd312177fe34cbd9db09c10
elazaroo <enekolazaroalba@gmail.com>
2024-09-16 08:17:31 +0200

    Added password empty and equal verifications

--- app/src/main/java/com/example/androidfirstapp/MainActivity.java
+++ app/src/main/java/com/example/androidfirstapp/MainActivity.java
@@ -7,18 +7,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private EditText etPassword,etRePassword;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            etPassword = findViewById(R.id.etPassword);
+            etRePassword = findViewById(R.id.etRePassword);
             return insets;
         });
     }
+
+    public void verify(View v)
+    {
+        String pass = etPassword.getText().toString();
+        if (pass.length()==0) {
+            Toast notificacion= Toast.makeText(this,"Password can't be empty", Toast.LENGTH_LONG);
+            notificacion.show();
+        }
+        verifyRePassword(v);
+    }
+
+    public void verifyRePassword(View v)
+{
+    String pass = etPassword.getText().toString();
+    String Repass = etRePassword.getText().toString();
+
+    if (!pass.equals(Repass))
+    {
+        Toast notificacion= Toast.makeText(this,"Passwords don't match", Toast.LENGTH_LONG);
+        notificacion.show();
+    }
+}
 }
