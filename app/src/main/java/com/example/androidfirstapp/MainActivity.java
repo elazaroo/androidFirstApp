ab3009af762024b7520726b7250dde4a9c7cabcb
elazaroo <enekolazaroalba@gmail.com>
2024-09-19 13:11:28 +0200

    Added functionality to = button

--- app/src/main/java/com/example/androidfirstapp/MainActivity.java
+++ app/src/main/java/com/example/androidfirstapp/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.androidfirstapp;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
 
@@ -72,5 +70,45 @@ public class MainActivity extends AppCompatActivity {
                 tvScreen.setText(currentText.substring(0, currentText.length() - 1));
             }
         }
+    }
+
+    public void equalButtonPressed(View view) {
+        TextView tvScreen = view.getRootView().findViewById(R.id.tvScreen);
+        if (tvScreen != null) {
+            String currentText = tvScreen.getText().toString();
+            if (!currentText.isEmpty()) {
+                tvScreen.setText(calculate(currentText));
+            }
+        }
+    }
+
+    private String calculate(String expression) {
+        String[] numbers = expression.split("(?<=[+x÷-])|(?=[+x÷-])");
+
+        double result = Double.parseDouble(numbers[0]);
+        for (int i = 1; i < numbers.length; i += 2) {
+            String operator = numbers[i];
+            double nextNumber = Double.parseDouble(numbers[i + 1]);
+            switch (operator) {
+                case "+":
+                    result += nextNumber;
+                    break;
+                case "-":
+                    result -= nextNumber;
+                    break;
+                case "x":
+                    result *= nextNumber;
+                    break;
+                case "÷":
+                    result /= nextNumber;
+                    break;
+            }
+        }
+
+        String resultString = (result == (int) result) ? String.valueOf((int) result) : String.valueOf(result);
+        if (resultString.length() > 17) {
+            resultString = String.format("%.5e", result); // Round to 5 decimal
+        }
+        return resultString.length() > 17 ? resultString.substring(0, 17) : resultString;
     }
 }
