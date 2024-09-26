5a55e8cf9ee9c7520c9835c380a21066e558813b
elazaroo <enekolazaroalba@gmail.com>
2024-09-26 13:06:02 +0200

    Revert "ListView with String"
    This reverts commit 1267bc68f7ea88cf5c01e2863c1df3189e1be0a1.

--- app/src/main/java/com/example/androidfirstapp/MainActivity.java
+++ app/src/main/java/com/example/androidfirstapp/MainActivity.java
@@ -1,11 +1,6 @@
 package com.example.androidfirstapp;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,31 +9,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] countries = { "Argentina", "Chile", "Paraguay", "Bolivia",
-            "Peru", "Ecuador", "Brasil", "Colombia", "Venezuela", "Uruguay" };
-    private String[] inhabitants = { "40000000", "17000000", "6500000",
-            "10000000", "30000000", "14000000", "183000000", "44000000",
-            "29000000", "3500000" };
-    private TextView tv1;
-    private ListView lv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        tv1=findViewById(R.id.tv1);
-        lv1=findViewById(R.id.list1);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, countries);
-        lv1.setAdapter(adapter);
-
-        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView adapterView, View view, int i, long l) {
-                tv1.setText("Población de "+ lv1.getItemAtPosition(i) + " es "+ inhabitants[i]);
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
     }
 }
