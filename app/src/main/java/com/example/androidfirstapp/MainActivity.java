095f139a383d8404a3e1785165617647395921a5
elazaroo <enekolazaroalba@gmail.com>
2024-09-30 11:40:43 +0200

    Toast Simple

--- app/src/main/java/com/example/androidfirstapp/MainActivity.java
+++ app/src/main/java/com/example/androidfirstapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidfirstapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText et1;
+    private int num;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        et1 = findViewById(R.id.etInput);
+
+        num=(int)(Math.random()*100001);
+        String collection = String.valueOf(num);
+        Toast notification = Toast.makeText(this,collection,Toast.LENGTH_LONG);
+        notification.show();
+    }
+
+    public void submit(View view) {
+        String enteredValue = et1.getText().toString();
+        int value = Integer.parseInt(enteredValue);
+        if (value==num) {
+            Toast notification=Toast.makeText(this,"Correct!.",Toast.LENGTH_LONG);
+            notification.show();
+        }
+        else {
+            Toast notification=Toast.makeText(this,"Incorrect.", Toast.LENGTH_LONG);
+            notification.show();
+        }
     }
 }
