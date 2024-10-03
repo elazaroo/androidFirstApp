8806e04661e0d73457dc6d9576b10ce3dd3e1d53
elazaroo <enekolazaroalba@gmail.com>
2024-10-03 12:44:50 +0200

    About activity

--- app/src/main/java/com/example/androidfirstapp/About.java
+++ app/src/main/java/com/example/androidfirstapp/About.java
@@ -1,6 +1,5 @@
 package com.example.androidfirstapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,16 +9,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class About extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
     }
 
-    public void about(View view) {
-        Intent i = new Intent(this, About.class );
-        startActivity(i);
+    public void exit(View v) {
+        finish();
     }
 }
