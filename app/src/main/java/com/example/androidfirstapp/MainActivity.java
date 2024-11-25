e696764702097b84baef3223a677384de0793c4e
elazaroo <enekolazaroalba@gmail.com>
2024-11-25 11:43:48 +0100

    MediaPlayer

--- app/src/main/java/com/example/androidfirstapp/MainActivity.java
+++ app/src/main/java/com/example/androidfirstapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidfirstapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void playLUV(View v) {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.luv);
+        mp.start();
     }
 }
