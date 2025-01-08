311f41c45f7ad65f595d7c37e7c2ed02764bf7ec
elazaroo <enekolazaroalba@gmail.com>
2025-01-08 08:32:04 +0100

    Yellow background & red 2 lines

--- app/src/main/java/com/example/androidfirstapp/MainActivity.java
+++ app/src/main/java/com/example/androidfirstapp/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.androidfirstapp;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +21,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout1), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            ConstraintLayout layout1 =findViewById(R.id.layout1);
+            CanvasX background = new CanvasX(this);
+            layout1.addView(background);
             return insets;
         });
+    }
+
+    class CanvasX extends View {
+        public CanvasX(Context context) {
+            super(context);
+        }
+
+        protected void onDraw(Canvas canvas) {
+            canvas.drawRGB(255, 255, 0);
+            int width = canvas.getWidth();
+            Paint brush1 = new Paint();
+            brush1.setARGB(255, 255, 0, 0);
+            canvas.drawLine(0, 30, width, 30, brush1);
+            brush1.setStrokeWidth(4);
+            canvas.drawLine(0, 60, width, 60, brush1);
+        }
     }
 }
