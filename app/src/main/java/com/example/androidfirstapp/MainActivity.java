fb527a47ce8649962cd6edbba49b65c468160076
elazaroo <enekolazaroalba@gmail.com>
2025-01-08 08:38:39 +0100

    3 ways to make rectangles

--- app/src/main/java/com/example/androidfirstapp/MainActivity.java
+++ app/src/main/java/com/example/androidfirstapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.androidfirstapp;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +20,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout1), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            ConstraintLayout layout1 = findViewById(R.id.layout1);
+            CanvasX bakground = new CanvasX(this);
+            layout1.addView(bakground);
+
             return insets;
         });
+    }
+
+    class CanvasX extends View {
+        public CanvasX(Context context) {
+            super(context);
+        }
+
+        protected void onDraw(Canvas canvas) {
+            canvas.drawRGB(255, 255, 255);
+            int width = canvas.getWidth();
+            Paint brush1 = new Paint();
+
+            brush1.setARGB(255,255,0,0);
+            canvas.drawRect(10, 10, width - 10, 40, brush1);
+
+            brush1.setStyle(Paint.Style.STROKE);
+            canvas.drawRect(10, 60, width -10, 90, brush1);
+
+            brush1.setStrokeWidth(3);
+            canvas.drawRect(10, 110, width -10, 140, brush1);
+        }
     }
 }
