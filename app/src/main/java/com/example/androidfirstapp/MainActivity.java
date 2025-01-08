059bac8850b10719724f2014f6232c74e125eeaa
elazaroo <enekolazaroalba@gmail.com>
2025-01-08 08:26:09 +0100

    Big dot

--- app/src/main/java/com/example/androidfirstapp/MainActivity.java
+++ app/src/main/java/com/example/androidfirstapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.androidfirstapp;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +20,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout1), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            ConstraintLayout layout1 = findViewById(R.id.layout1);
+            CanvasX background = new CanvasX(this);
+            layout1.addView(background);
             return insets;
         });
+    }
+
+    class CanvasX extends View {
+        public CanvasX(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            int width = canvas.getWidth();
+            int height = canvas.getHeight();
+            Paint brush1 = new Paint();
+            brush1.setARGB(255, 0, 0, 0);
+            brush1.setStrokeWidth(20); // Increase dot size
+            canvas.drawPoint(width / 2, height / 2, brush1);
+        }
     }
 }
