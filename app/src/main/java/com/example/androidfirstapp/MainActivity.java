a5ab561566861c70ccd13837e5ce88ace10bc838
elazaroo <enekolazaroalba@gmail.com>
2025-01-17 08:24:40 +0100

    Path

--- app/src/main/java/com/example/androidfirstapp/MainActivity.java
+++ app/src/main/java/com/example/androidfirstapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
@@ -44,21 +45,22 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onDraw(Canvas canvas) {
             canvas.drawRGB(0, 0, 255);
-            Paint brush1 = new Paint();
-            brush1.setARGB(255, 255, 0, 0);
-            brush1.setTextSize(30);
-            brush1.setTypeface(Typeface.SERIF);
-            canvas.drawText("Hello World (SERIF)", 0, 70, brush1);
-            brush1.setTypeface(Typeface.SANS_SERIF);
-            canvas.drawText("Hello World (SANS SERIF)", 0, 100, brush1);
-            brush1.setTypeface(Typeface.MONOSPACE);
-            canvas.drawText("Hello World (MONOSPACE)", 0, 140, brush1);
-            Typeface tf = Typeface.create(Typeface.SERIF, Typeface.ITALIC);
-            brush1.setTypeface(tf);
-            canvas.drawText("Hello World (SERIF ITALIC)", 0, 180, brush1);
-            tf = Typeface.create(Typeface.SERIF, Typeface.ITALIC | Typeface.BOLD);
-            brush1.setTypeface(tf);
-            canvas.drawText("Hello World (SERIF ITALIC BOLD)", 0, 220, brush1);
+            int height = canvas.getHeight();
+
+            Path path = new Path();
+            path.moveTo(0, height / 2);
+            path.lineTo(40, height / 2 - 30);
+            path.lineTo(80, height / 2 - 60);
+            path.lineTo(120, height / 2 - 90);
+            path.lineTo(160, height / 2 - 120);
+            path.lineTo(220, height / 2 - 150);
+            path.lineTo(280, height / 2 - 180);
+            path.lineTo(340, height / 2 - 210);
+            Paint pincel1 = new Paint();
+            pincel1.setARGB(255, 255, 0, 0);
+            pincel1.setTextSize(50);
+            canvas.drawTextOnPath("Hello World Hello World", path, 0, 0,
+                    pincel1);
         }
     }
 }
